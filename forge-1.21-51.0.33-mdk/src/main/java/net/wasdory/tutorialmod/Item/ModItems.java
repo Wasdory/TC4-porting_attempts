1f0b73610e61695d84f35c2d1b51532f87d07b34
Zabis13 <lbsbmsu@mail.ru>
2025-12-16 22:12:00 +0300

    Thaumium and Void metal

---
+++ forge-1.21-51.0.33-mdk/src/main/java/net/wasdory/tutorialmod/Item/ModItems.java
@@ -0,0 +1,24 @@
+package net.wasdory.tutorialmod.Item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.wasdory.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> THAUMIUM = ITEMS.register("thaumium",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> VOID_METAL = ITEMS.register("void_metal",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
--- forge-1.21-51.0.33-mdk/src/main/java/net/wasdory/tutorialmod/TutorialMod.java
+++ forge-1.21-51.0.33-mdk/src/main/java/net/wasdory/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.wasdory.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +15,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.wasdory.tutorialmod.Item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -29,6 +32,8 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -41,6 +46,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.VOID_METAL);
+            event.accept(ModItems.THAUMIUM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
