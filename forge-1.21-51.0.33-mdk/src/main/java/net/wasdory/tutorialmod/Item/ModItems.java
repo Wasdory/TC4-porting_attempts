36818144386a98bd9a1b03e2653b498894e87ffc
Zabis13 <lbsbmsu@mail.ru>
2025-12-23 16:58:07 +0300

    Tags and Tooltips

--- forge-1.21-51.0.33-mdk/src/main/java/net/wasdory/tutorialmod/Item/ModItems.java
+++ forge-1.21-51.0.33-mdk/src/main/java/net/wasdory/tutorialmod/Item/ModItems.java
@@ -1,6 +1,9 @@
 package net.wasdory.tutorialmod.Item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,6 +12,8 @@ import net.wasdory.tutorialmod.Item.custom.ChiselItem;
 import net.wasdory.tutorialmod.Item.custom.FuelItem;
 import net.wasdory.tutorialmod.TutorialMod;
 
+import java.util.List;
+
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
@@ -16,7 +21,13 @@ public class ModItems {
     public static final RegistryObject<Item> THAUMIUM = ITEMS.register("thaumium",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> VOID_METAL = ITEMS.register("void_metal",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties()) {
+                @Override
+                public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+                    pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.void_metal"));
+                    super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+                }
+            });
     public static final RegistryObject<Item> AMBER = ITEMS.register("amber",
             () -> new Item(new Item.Properties()));
 
